613d0f2ae8ad9e9d03b4a6024b250395849520c0
Robert Andrew Stevens <robertandrewstevens@gmail.com>
2016-03-20 21:52:13 -0500

    update
    add files

---
+++ code.sql
@@ -0,0 +1,72 @@
+-- ex2.sql
+
+CREATE TABLE person (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE pet (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT,
+    age INTEGER,
+    dead INTEGER
+);
+
+CREATE TABLE person_pet (
+    person_id INTEGER,
+    pet_id INTEGER
+);
+
+-- ex3.sql
+
+INSERT INTO person (id, first_name, last_name, age)
+    VALUES (0, "Zed", "Shaw", 37);
+
+INSERT INTO pet (id, name, breed, age, dead)
+    VALUES (0, "Fluffy", "Unicorn", 1000, 0);
+
+INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
+
+-- ex4.sql
+
+INSERT INTO person_pet (person_id, pet_id) VALUES (0, 0);
+INSERT INTO person_pet VALUES (0, 1);
+
+-- ex5.sql
+
+SELECT * FROM person;
+
+SELECT name, age FROM pet;
+
+SELECT name, age FROM pet WHERE dead = 0;
+
+SELECT * FROM person WHERE first_name != "Ze
+
+-- ex6.sql
+
+SELECT pet.id, pet.name, pet.age, pet.dead
+    FROM pet, person_pet, person
+    WHERE
+    pet.id = person_pet.pet_id AND
+    person_pet.person_id = person.id AND
+    person.first_name = "Zed";
+    
+-- ex7.sql
+
+/* make sure there's dead pets */
+SELECT name, age FROM pet WHERE dead = 1;
+
+/* aww poor robot */
+DELETE FROM pet WHERE dead = 1;
+
+/* make sure the robot is gone */
+SELECT * FROM pet;
+
+/* let's resurrect the robot */
+INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 0);
+
+/* the robot LIVES! */
+SELECT * FROM pet;
